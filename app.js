0efb76f263a569f698068675043d8f3ee3c164c0
Vishal Rawat <omniverserawat2005@gmail.com>
2024-03-10 12:07:01 +0530

    Add new files

---
+++ app.js
@@ -0,0 +1,19 @@
+let btns = document.querySelectorAll(".circle button");
+let circle =document.querySelector(".circle");
+let main = document.querySelector(".main");
+let nav = document.querySelector(".navbar");
+
+btns[0].addEventListener("click", ()=>{
+    circle.classList.add("rotate3");
+    circle.classList.remove("rotate2");
+    main.id="anti-rotate" ;
+    nav.classList.add("hide");
+
+})
+btns[1].addEventListener("click", ()=>{
+    circle.classList.add("rotate2");
+    circle.classList.remove("rotate3");
+    main.id="rotate" ;
+    nav.classList.add("show");
+    nav.classList.remove("hide");
+})
